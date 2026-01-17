dc7bbfa900a38c6b1960db03d575db1fb19e6e7b
Bruno Henrique <62818096+brunons2014@users.noreply.github.com>
2026-01-17 01:45:47 -0300

    Criando uma rota

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from main import RegistroClima  # Importa a classe que você já criou
+
+app = FastAPI()
+# Conecta ao banco (no Docker use 'db', local use 'localhost')
+engine = create_engine('postgresql://usuario:senha@localhost:5432/clima_db')
+
+@app.get("/clima")
+def buscar_clima():
+    with Session(engine) as session:
+        # Busca todos os registros do banco
+        statement = select(RegistroClima).order_by(RegistroClima.data_hora.desc())
+        resultados = session.execute(statement).scalars().all()
+        return resultados
